a5f3331b746e8a5539d165b86f363ca06ab709b7
xiesheaeix <sheae123@gmail.com>
2022-12-28 11:18:51 -0800

    first commit

---
+++ src/components/Banner.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import { Container, Row ,Col } from "react-bootstrap";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+
+export default function Banner() {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Web Developer", "Full-Stack", "Web Design"];
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const timePeriod = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        },delta)
+
+        return () => {clearInterval(ticker)};
+    }, [text]);
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2)
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(timePeriod);
+        } else if (isDeleting && updatedText === '')  {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+
+
+    }
+
+    return (
+        <section className="banner" id="home">
+            <video autoPlay muted loop>
+             <source src="https://i.imgur.com/Gyy22GJ.mp4" type="video/mp4" />
+            </video>
+            <Container>
+                <Row className="align-items-center">
+                    <Col xs={12} md={6} xl={7}>
+                        <span className="tagline">Welcome to my portfolio</span>
+                        <h1>{`Hi I'm Shea Ehrenberger `}<br></br><span className="wrap">{text}</span></h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi culpa tenetur tempore porro eum cum natus hic enim, vitae accusamus eveniet temporibus similique quisquam quam unde. Eligendi officia quod ipsum.</p>
+                        <button onClick={() => console.log('connect')}>Let's Connect <ArrowRightCircle size={25}/> </button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src='https://i.imgur.com/SA3phdf.png' alt="header img" />
+                    </Col>
+                </Row>
+            </Container>
+
+        </section>
+    );
+}
---
+++ src/pages/App/App.js
@@ -0,0 +1,15 @@
+import NavBar from '../../components/NavBar';
+import Banner from '../../components/Banner';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
