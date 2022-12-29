087357a3a9354312ca1aad9b656db07dff2f11d3
xiesheaeix <sheae123@gmail.com>
2022-12-28 17:54:34 -0800

    trying to fix delpoyment

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(3001, () => console.log("Server Running"));
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => console.log("Server Running"));
 // console.log(process.env.EMAIL_USER);
 // console.log(process.env.EMAIL_PASS);
 
